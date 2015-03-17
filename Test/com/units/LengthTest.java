83cfb7455755368e1bd56ca9a092804d299c1274
Dolly3g <dollygyanchandani1996@gmail.com>
2015-03-17 09:12:43 +0530

    volume can also be compared. Scalar Quantity added.

--- Test/com/units/LengthTest.java
+++ Test/com/units/LengthTest.java
@@ -6,79 +6,60 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-/**
- * Created by saylik on 3/13/2015.
- */
 public class LengthTest {
     @Test
     public void equalsReturnsTrueIfLengthsOfSameUnitsAreEqual(){
-        Length l1 = new Length(1,"feet");
-        Length l2 = new Length(1,"feet");
+        Length l1 = new Length(1,Unit.FEET);
+        Length l2 = new Length(1,Unit.FEET);
         assertTrue(l1.equals(l2));
     }
 
     @Test
     public void equalsReturnsFalseIfLengthsOfSameUnitsAreNotEqual(){
-        Length l1 = new Length(1,"feet");
-        Length l2 = new Length(2,"feet");
+        Length l1 = new Length(1,Unit.FEET);
+        Length l2 = new Length(2,Unit.FEET);
         assertFalse(l1.equals(l2));
     }
 
     @Test
     public void equalsReturnsTrueIfLengthsOfFeetAndInchesAreEqual(){
-        Length l1 = new Length(1,"feet");
-        Length l2 = new Length(12,"inches");
+        Length l1 = new Length(1,Unit.FEET);
+        Length l2 = new Length(12,Unit.INCH);
         assertTrue(l1.equals(l2));
     }
 
     @Test
     public void equalsReturnsTrueIfLengthsOfInchesAndFeetAreEqual(){
-        Length l1 = new Length(1,"feet");
-        Length l2 = new Length(12,"inches");
+        Length l1 = new Length(1,Unit.FEET);
+        Length l2 = new Length(12,Unit.INCH);
         assertTrue(l2.equals(l1));
     }
 
     @Test
     public void equalsReturnsFalseIfLengthsOfInchesAndFeetAreNotEqual(){
-        Length l1 = new Length(1,"feet");
-        Length l2 = new Length(13,"inches");
+        Length l1 = new Length(1,Unit.FEET);
+        Length l2 = new Length(13,Unit.INCH);
         assertFalse(l1.equals(l2));
     }
 
     @Test
     public void equalsReturnsFalseIfLengthsOfFeetAndInchesAreNotEqual(){
-        Length l1 = new Length(2,"feet");
-        Length l2 = new Length(12,"inches");
+        Length l1 = new Length(2,Unit.FEET);
+        Length l2 = new Length(12,Unit.INCH);
         assertFalse(l1.equals(l2));
     }
 
     @Test
     public void equalsReturnsFalseIfLengthsOfFeetAndInchesAreEqual(){
-        Length l1 = new Length(24,"inches");
-        Length l2 = new Length(2,"feet");
+        Length l1 = new Length(24,Unit.INCH);
+        Length l2 = new Length(2,Unit.FEET);
         assertTrue(l1.equals(l2));
-    }
-
-    @Test
-    public void convertToInchConvertsFeetToInch(){
-        Length l1 = new Length(1,"feet");
-        Length l2 = new Length(12,"inches");
-        assertTrue(l2.equals(l1.convertToInch()));
-    }
-
-    @Test
-    public void convertToInchConvertsFeetToCentimeters(){
-        Length l1 = new Length(1,"centimeters");
-        Length expected = new Length(0.39,"inches");
-        assertTrue(expected.equals(l1.convertToInch()));
     }
 
     @Test
     public void equalsReturnsTrueIfLengthsOfInchesAndCentimetersAreEqual(){
-        Length l1 = new Length(1,"inches");
-        Length l2 = new Length(2.54,"centimeters");
+        Length l1 = new Length(1,Unit.INCH);
+        Length l2 = new Length(2.54,Unit.CENTIMETERS);
         assertTrue(l1.equals(l2));
     }
-
-
 }
--- src/com/units/Length.java
+++ src/com/units/Length.java
@@ -1,52 +1,21 @@
 package com.units;
 
 import java.lang.String;
-import java.util.DoubleSummaryStatistics;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by saylik on 3/13/2015.
  */
-public class Length {
-    double value;
-    String unit;
-
-    private static Map<String ,Double> inchConversionFactors = new HashMap<String, Double>();
-
+public class Length extends ScalarQuantity{
     static {
-        inchConversionFactors.put("feet",12.0);
-        inchConversionFactors.put("centimeters",0.393701);
+        conversionFactors.put(Unit.MILLIMETER, 1.0);
+        conversionFactors.put(Unit.FEET, 304.8);
+        conversionFactors.put(Unit.CENTIMETERS,10.0);
+        conversionFactors.put(Unit.INCH,25.4);
     }
 
-    private static final double PRECISION = 0.001;
-
-    public Length(double value, String unit) {
-        this.value = value;
-        this.unit = unit;
-    }
-
-    public boolean equals(Length l2) {
-        Length l1 = this;
-        if(l1.unit != "inches"){
-            l1 = l1.convertToInch();
-        }
-        if(l2.unit != "inches"){
-            l2 = l2.convertToInch();
-        }
-        return Math.abs(l1.value - l2.value) < PRECISION && l1.unit == l2.unit;
-    }
-
-    public Length convertToInch() {
-        double factor = inchConversionFactors.get(unit);
-        return new Length(value*factor,"inches");
-    }
-
-    @Override
-    public String toString() {
-        return "Length{" +
-                "value=" + value +
-                ", unit='" + unit + '\'' +
-                '}';
+    public Length(double value, Unit unit) {
+        super(value, unit);
     }
 }
